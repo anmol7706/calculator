7297ec3817dce6d030a6a59d6fa15e07da5f3f47
anmol7706 <anmol770693@gmail.com>
2021-11-25 14:36:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+let screen = document.getElementById('display');
+buttons = document.querySelectorAll('button');
+let currentScreenValue = '';
+let previousScreenValue = '';
+
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        if (buttonText == 'X') {
+            buttonText = '*';
+            currentScreenValue += buttonText;
+            previousScreenValue = currentScreenValue;
+            screen.value = previousScreenValue;
+        }
+        else if (buttonText == 'C') {
+            currentScreenValue = "";
+            screen.value = currentScreenValue;
+        }
+        else if (buttonText == 'keyboard_backspace') {
+            screen.value = screen.value.slice(0, -1);
+            currentScreenValue = screen.value;
+        }
+        else if (buttonText == '=') {
+            previousScreenValue = currentScreenValue;
+            let x = eval(previousScreenValue);
+            screen.value = x;
+            currentScreenValue = x;
+        }
+        else {
+            currentScreenValue += buttonText;
+            screen.value = currentScreenValue;
+        }
+
+    })
+}
+
