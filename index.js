6fd2fc33d7ee0118c48a56b3d5a17d105070da06
anmol7706 <anmol770693@gmail.com>
2021-11-25 14:43:08 +0530

    comment

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let screen = document.getElementById('display');
 buttons = document.querySelectorAll('button');
 let currentScreenValue = '';
 let previousScreenValue = '';
-
-for (item of buttons) {
+// loop for buttons
+for (item of buttons) { 
     item.addEventListener('click', (e) => {
         buttonText = e.target.innerText;
         console.log('Button text is ', buttonText);
@@ -13,10 +13,12 @@ for (item of buttons) {
             previousScreenValue = currentScreenValue;
             screen.value = previousScreenValue;
         }
+        // clear the screen
         else if (buttonText == 'C') {
             currentScreenValue = "";
             screen.value = currentScreenValue;
         }
+        // deleting the last text
         else if (buttonText == 'keyboard_backspace') {
             screen.value = screen.value.slice(0, -1);
             currentScreenValue = screen.value;
